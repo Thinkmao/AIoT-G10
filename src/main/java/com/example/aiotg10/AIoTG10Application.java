d6a2db23b5f719db23837ca87e14a4d2bfbbce50
Thinkmiao <dongguawxz@sohu.com>
2023-07-12 14:57:22 +0800

    base

---
+++ src/main/java/com/example/aiotg10/AIoTG10Application.java
@@ -0,0 +1,13 @@
+package com.example.aiotg10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AIoTG10Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AIoTG10Application.class, args);
+    }
+
+}
