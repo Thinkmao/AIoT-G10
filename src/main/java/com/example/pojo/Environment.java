c8990b4a9453cf7921efa4c886de94d58d5c436a
Ordinary <ordinary021001@qq.com>
2023-07-14 15:56:31 +0800

    first commit

---
+++ src/main/java/com/example/pojo/Environment.java
@@ -0,0 +1,27 @@
+package com.example.pojo;
+
+public class Environment
+{
+    private Double temperature;
+    private Double humidity;
+
+    public Double getTemperature()
+    {
+        return temperature;
+    }
+
+    public void setTemperature(Double temperature)
+    {
+        this.temperature = temperature;
+    }
+
+    public Double getHumidity()
+    {
+        return humidity;
+    }
+
+    public void setHumidity(Double humidity)
+    {
+        this.humidity = humidity;
+    }
+}
---
+++ src/main/java/com/example/pojo/Posture.java
@@ -0,0 +1,16 @@
+package com.example.pojo;
+
+public class Posture
+{
+    private Integer status;
+
+    public Integer getStatus()
+    {
+        return status;
+    }
+
+    public void setStatus(Integer status)
+    {
+        this.status = status;
+    }
+}
